abe57043667525d21b2e4c4be042a6cd9ca4fa89
masa1203 <msys.sawada@gmail.com>
2021-10-25 21:34:54 +0900

    Linkタグを試してみた

--- next-portfolio/src/pages/index.js
+++ next-portfolio/src/pages/index.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import * as style from "../styles/index.module.css";
 
 const Index = () => {
   return (
     <div>
       <h1 className={style.h1Text}>こんにちは</h1>
+      <Link href="/contact">
+        <a>Contactページへ移動</a>
+      </Link>
     </div>
   );
 };
