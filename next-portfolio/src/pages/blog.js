3dc96d6d9ec17f6943912a7edca35614016cb64e
masa1203 <msys.sawada@gmail.com>
2021-10-25 21:53:16 +0900

    getStaticProps()関数でコンポーネントへデータを渡せる

--- next-portfolio/src/pages/blog.js
+++ next-portfolio/src/pages/blog.js
@@ -1,11 +1,22 @@
 import React from "react";
 
-const Blog = () => {
+const Blog = (props) => {
+  console.log(props);
   return (
     <div>
       <h1>ブログページ</h1>
+      <a>{props.test}</a>
     </div>
   );
 };
 
 export default Blog;
+
+export async function getStaticProps() {
+  const testText = "Next.jsポートフォリオサイト";
+  return {
+    props: {
+      test: testText,
+    },
+  };
+}
