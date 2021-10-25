69837b1d67039275b132b7b1ad5c648fb5679de7
masa1203 <msys.sawada@gmail.com>
2021-10-25 21:30:31 +0900

    styleの適用をしてみる

--- next-portfolio/src/pages/index.js
+++ next-portfolio/src/pages/index.js
@@ -1,7 +1,9 @@
+import * as style from "../styles/index.module.css";
+
 const Index = () => {
   return (
     <div>
-      <h1>こんにちは</h1>
+      <h1 className={style.h1Text}>こんにちは</h1>
     </div>
   );
 };
